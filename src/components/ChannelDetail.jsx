393528f67046712a9ffd0d35d7c3674bfa2543e4
Nícolas Brandão <107960652+nicolasbrandao@users.noreply.github.com>
2023-02-09 17:30:25 -0300

    First working version

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,39 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Box } from "@mui/material";
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+
+const ChannelDetail = () => {
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+      .then((data) => setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`)
+      .then((data) => setVideos(data?.items));
+  }, [id]);
+
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{
+            background: "radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 26%, rgba(0,212,255,1) 100%)",
+            zIndex: 10,
+            height: "300px"
+          }} 
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop="-130px"/>
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        <Videos videos={videos} />
+      </Box>
+    </Box>
+  )
+}
+
+export default ChannelDetail
